381b0097ba89b8bc13ad4cfa71f975b75945158a
hydev777 <hytechwilson@gmail.com>
2024-04-08 11:22:32 -0400

    changed name of view to page

--- lib/router.dart
+++ lib/router.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meals_repository/meals_repository.dart';
 
-import 'category_meals/view/meals_page.dart';
-import 'meal_categories/view/categories_page.dart';
-import 'category_meals/view/meal_details_page.dart';
+import 'category_meals/pages/meals_page.dart';
+import 'meal_categories/pages/categories_page.dart';
+import 'category_meals/pages/meal_details_page.dart';
 
 final _rootKey = GlobalKey<NavigatorState>();
 final _nestedKey = GlobalKey<NavigatorState>();
